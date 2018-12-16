073b044580a076fcdff2b39fe47e3eb9cd90abd7
JayBee <jemallbrown@gmail.com>
2018-12-16 11:47:37 -0500

    pushing schema and initial file structure

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+-- 3. Still in the `db` folder, create a `seeds.sql` file. 
+-- In this file, write insert queries to populate the `burgers` table with at least three entries.
+
+INSERT INTO burgers (burger_name,devoured) VALUES ('Big Mac', false);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Whopper', false);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Bacon Double Cheese', false);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Turkey Burger', true);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (burger_name,devoured) VALUES ('California Burger', true);
+
+
