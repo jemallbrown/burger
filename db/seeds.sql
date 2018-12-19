1d8a536f96caeb90ed2b10656596decb15db428c
JayBee <jemallbrown@gmail.com>
2018-12-19 18:45:53 -0500

    fixed database to drop table if exists

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,3 @@
--- 3. Still in the `db` folder, create a `seeds.sql` file. 
--- In this file, write insert queries to populate the `burgers` table with at least three entries.
 USE burgers_db;
 
 INSERT INTO burgers (burger_name,devoured) VALUES ('Big Mac', false);
